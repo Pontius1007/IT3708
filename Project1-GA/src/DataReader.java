b12fcd8306917304e8d218897a9f8babf47f5561
Pontius1007 <embrikeinang@msn.com>
2019-02-04 15:05:46 +0100

    Add neighbourMatrix

--- Project1-GA/src/DataReader.java
+++ Project1-GA/src/DataReader.java
@@ -1,8 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.DecimalFormat;
+import java.util.*;
 
 public class DataReader {
     private int number_of_vehicles_per_depot;
@@ -12,6 +12,7 @@ public class DataReader {
     private Map<Integer, Vehicle> vehicle_dict = new HashMap<Integer, Vehicle>();
     private Map<Integer, Customer> customer_dict = new HashMap<Integer, Customer>();
     private Map<Integer, Depot> depot_dict = new HashMap<Integer, Depot>();
+    private List<List<Double>> neighBourMatrix = new ArrayList<>();
 
     private DataReader() {
     }
@@ -33,37 +34,82 @@ public class DataReader {
             for (int depot = 0; depot < this.number_of_depots; depot++) {
                 line = br.readLine();
                 splitStr = line.trim().split("\\s+");
-                int maxDuration = Integer.parseInt(splitStr[0]);
+                int maxDistance = Integer.parseInt(splitStr[0]) == 0 ? Integer.MAX_VALUE: Integer.parseInt(splitStr[0]);
                 int maxLoad = Integer.parseInt(splitStr[1]);
                 for (int vehicle = 0; vehicle < this.number_of_vehicles_per_depot; vehicle++) {
-                    int veID = (depot * this.number_of_vehicles_per_depot + vehicle) + 1;
-                    vehicle_dict.put(veID, new Vehicle(depot, veID, maxDuration, maxLoad));
+                    int veID = (depot * this.number_of_vehicles_per_depot + vehicle);
+                    vehicle_dict.put(veID, new Vehicle(depot, veID, maxDistance, maxLoad));
                 }
             }
 
             // Create the customers
-            for (int customer_id = 1; customer_id <= this.number_of_costumers; customer_id++) {
+            for (int customer_id = 0; customer_id < this.number_of_costumers; customer_id++) {
                 line = br.readLine();
                 splitStr = line.trim().split("\\s+");
-                customer_dict.put(Integer.parseInt(splitStr[0]), new Customer(Integer.parseInt(splitStr[1]),
-                        Integer.parseInt(splitStr[2]), Integer.parseInt(splitStr[3]), Integer.parseInt(splitStr[4])));
+                customer_dict.put(customer_id, new Customer(Integer.parseInt(splitStr[1]),
+                        Integer.parseInt(splitStr[2]), Integer.parseInt(splitStr[3]), Integer.parseInt(splitStr[4]),
+                        Integer.parseInt(splitStr[0])));
             }
 
             // Create depots
-            for (int depot_id = 1; depot_id <= this.number_of_depots; depot_id++) {
+            for (int depot_id = 0; depot_id < this.number_of_depots; depot_id++) {
                 line = br.readLine();
                 splitStr = line.trim().split("\\s+");
                 depot_dict.put(depot_id, new Depot(Integer.parseInt(splitStr[1]), Integer.parseInt(splitStr[2])));
             }
         } finally {
             br.close();
+            this.createNeighbourMatrix();
+            DNA.neightbourMatrix = this.neighBourMatrix;
+        }
+    }
+
+    private void createNeighbourMatrix() {
+        DecimalFormat df = new DecimalFormat("#.00");
+        for (int customer = 0; customer < this.customer_dict.size(); customer++) {
+            this.neighBourMatrix.add(new ArrayList<Double>());
+            //String resultline = "";
+            for (int customer2 = 0; customer2 < this.customer_dict.size(); customer2++) {
+                double distance = Math.sqrt(Math.pow(this.customer_dict.get(customer2).getX() - this.customer_dict.get(customer).getX(), 2) +
+                        Math.pow(this.customer_dict.get(customer2).getY() - this.customer_dict.get(customer).getY(), 2));
+
+                this.neighBourMatrix.get(customer).add(distance);
+                //resultline += (" " + df.format(distance));
+            }
+            for (int depot = 0; depot < this.depot_dict.size(); depot++) {
+                double distance = Math.sqrt(Math.pow(this.depot_dict.get(depot).getX() - this.customer_dict.get(customer).getX(), 2) +
+                        Math.pow(this.depot_dict.get(depot).getY() - this.customer_dict.get(customer).getY(), 2));
+                this.neighBourMatrix.get(customer).add(distance);
+                //resultline += (" " + df.format(distance));
+            }
+            //System.out.println(resultline);
+        }
+        for (int depot = 0; depot < this.depot_dict.size(); depot++) {
+            String resultline = "";
+            this.neighBourMatrix.add(new ArrayList<Double>());
+
+            for (int customer2 = 0; customer2 < this.customer_dict.size(); customer2++) {
+                double distance = Math.sqrt(Math.pow(this.customer_dict.get(customer2).getX() - this.depot_dict.get(depot).getX(), 2) +
+                        Math.pow(this.customer_dict.get(customer2).getY() - this.depot_dict.get(depot).getY(), 2));
+
+                this.neighBourMatrix.get(customer_dict.size()+depot).add(distance);
+                //resultline += (" " + df.format(distance));
+
+            }
+            for (int depot2 = 0; depot2 < this.depot_dict.size(); depot2++) {
+                double distance = Math.sqrt(Math.pow(this.depot_dict.get(depot).getX() - this.customer_dict.get(depot2).getX(), 2) +
+                        Math.pow(this.depot_dict.get(depot).getY() - this.customer_dict.get(depot2).getY(), 2));
+                this.neighBourMatrix.get(customer_dict.size()+depot).add(distance);
+            }
+            //System.out.println(resultline);
         }
     }
 
     public static void main(String[] args) throws IOException {
         DataReader dr = new DataReader();
-        dr.readFile("data/Data Files/p04");
+        dr.readFile("data/Data Files/p01");
         Visualizer vis = new Visualizer(dr.depot_dict, dr.customer_dict);
+        DNA dna = new DNA(dr.vehicle_dict, dr.depot_dict, dr.customer_dict);
     }
 }
 
