2949e55df9c831e05b3609e1cebdf0d54b973c8f
Pontius1007 <embrikeinang@msn.com>
2019-02-05 18:00:08 +0100

    WIP

--- Project1-GA/src/DNA.java
+++ Project1-GA/src/DNA.java
@@ -14,42 +14,45 @@ public class DNA {
     private double punnishmentRate = 100;
 
     public static List<List<Double>> neightbourMatrix;
+    private static Map<Integer, Vehicle> vehicles;
+    private static Map<Integer, Depot> depots;
+    private static Map<Integer, Customer> customers;
 
-    public DNA(Map<Integer, Vehicle> vehicles, Map<Integer, Depot> depots, Map<Integer, Customer> customers) {
-        DNAString = new ArrayList<>();
-        vehicleWeights = new ArrayList<>();
-        vehicleDistance = new ArrayList<>();
-        initializeDnaRandomly(vehicles, depots, customers);
+    public DNA() {
+        this.DNAString = new ArrayList<>();
+        this.vehicleWeights = new ArrayList<>();
+        this.vehicleDistance = new ArrayList<>();
+        initializeDnaRandomly();
         /* System.out.println("Distance: ");
         System.out.println(this.totalDistance);
         System.out.println("Fitness: ");
         System.out.println(this.fitness); */
     }
 
-    public void initializeDnaRandomly(Map<Integer, Vehicle> vehicles, Map<Integer, Depot> depots, Map<Integer, Customer> customers){
+    public void initializeDnaRandomly(){
         List<Integer> possibleVehicles = new ArrayList<>();
-        for (int x = 0; x < vehicles.size(); x++){
-            DNAString.add(new ArrayList<>());
-            vehicleWeights.add(vehicles.get(x).getMaxLoad());
+        for (int x = 0; x < this.vehicles.size(); x++){
+            this.DNAString.add(new ArrayList<>());
+            this.vehicleWeights.add(this.vehicles.get(x).getMaxLoad());
             possibleVehicles.add(x);
         }
         //add all customers randomly
-        for (Customer customer: customers.values()) {
-            addCustomer(customer, customers, vehicles, possibleVehicles);
+        for (Customer customer: this.customers.values()) {
+            addCustomer(customer, possibleVehicles);
         }
 
         //add closest ending depot for each vehicle
-        for(int routeID = 0; routeID < DNAString.size(); routeID++){
+        for(int routeID = 0; routeID < this.DNAString.size(); routeID++){
             List<Integer> route = this.DNAString.get(routeID);
             int closestDepotId = 0;
             double closestDepotDistance = Double.MAX_VALUE;
-            for(int i = 0; i < depots.size(); i++){
+            for(int i = 0; i < this.depots.size(); i++){
                 if(route.size() == 0){
-                    closestDepotId = vehicles.get(routeID).getDepotID();
+                    closestDepotId = this.vehicles.get(routeID).getDepotID();
                     closestDepotDistance = 0;
                 }
                 else{
-                    double currentDistance = neightbourMatrix.get(route.get(route.size()-1)).get(customers.size()+i);
+                    double currentDistance = neightbourMatrix.get(route.get(route.size()-1)).get(this.customers.size()+i);
                     if(currentDistance < closestDepotDistance){
                         closestDepotId = i;
                         closestDepotDistance = currentDistance;
@@ -57,7 +60,18 @@ public class DNA {
                 }
             }
             route.add(closestDepotId);
-            this.punnishment += Math.max(0, calculateRouteLength(route, vehicles.get(routeID).getDepotID(), customers.size())-vehicles.get(routeID).getMaxDuration());
+            this.punnishment += Math.max(0, calculateRouteLength(route, this.vehicles.get(routeID).getDepotID(), this.customers.size())-this.vehicles.get(routeID).getMaxDuration());
+        }
+        this.punnishment *= this.punnishmentRate;
+        this.totalDistance = this.calculateFitness(this.vehicles, this.customers.size());
+        this.fitness = this.totalDistance + this.punnishment;
+    }
+
+    public void updateFitness(){
+        for(int routeID = 0; routeID < DNAString.size(); routeID++){
+            List<Integer> route = this.DNAString.get(routeID);
+            this.punnishment += Math.max(0, calculateRouteLength(route, this.vehicles.get(routeID).getDepotID(),
+                    this.customers.size())-this.vehicles.get(routeID).getMaxDuration());
         }
         this.punnishment *= this.punnishmentRate;
         this.totalDistance = this.calculateFitness(vehicles, customers.size());
@@ -65,19 +79,19 @@ public class DNA {
     }
 
 
-    private void addCustomer(Customer customer, Map<Integer, Customer> customers, Map<Integer, Vehicle> vehicles, List<Integer> possibleVehicles){
+    private void addCustomer(Customer customer, List<Integer> possibleVehicles){
         if(possibleVehicles.size() == 0){
             throw new IllegalStateException("No more room in any vehicles (No room for weight in any vehicle)");
         }
         int randomVehicleIdx = ThreadLocalRandom.current().nextInt(0, possibleVehicles.size());
         int randomVehicle = possibleVehicles.get(randomVehicleIdx);
-        double routeWeight = this.testRouteWeight(customers, this.DNAString.get(randomVehicle), customer);
+        double routeWeight = this.testRouteWeight(this.DNAString.get(randomVehicle), customer);
         if (customer.getWeight() < vehicleWeights.get(randomVehicle)) {
             this.DNAString.get(randomVehicle).add(customer.getCustomerID());
         }
         else{
             possibleVehicles.remove(randomVehicleIdx);
-            addCustomer(customer, customers, vehicles, possibleVehicles);
+            addCustomer(customer, possibleVehicles);
         }
     }
 
@@ -92,19 +106,19 @@ public class DNA {
     }
 
 
-    private double calculateFitness(Map<Integer, Vehicle> vehicles, int customerSize){
+    private double calculateFitness(){
         double fitness = 0;
         for(int i = 0; i < this.DNAString.size(); i++){
             List<Integer> route = this.DNAString.get(i);
-            fitness += calculateRouteLength(route, vehicles.get(i).getDepotID(), customerSize);
+            fitness += calculateRouteLength(route, this.vehicles.get(i).getDepotID());
         }
         return fitness;
     }
 
-    private double calculateRouteLength(List<Integer> route, int startDepotId, int customerSize) {
+    private double calculateRouteLength(List<Integer> route, int startDepotId) {
         double distance = 0;
         //This is the index of the starting depot in the neighbour matrix
-        int previous = customerSize-1 + startDepotId;
+        int previous = this.customers.size()-1 + startDepotId;
         //Adds distance between all customers
         for(int i = 0; i < route.size() - 1; i++) {
             int current = route.get(i);
@@ -112,16 +126,16 @@ public class DNA {
             previous = current;
         }
         //Adds distance from last customer to end depot
-        int current = customerSize-1 + route.get(route.size()-1);
+        int current = this.customers.size()-1 + route.get(route.size()-1);
             distance += neightbourMatrix.get(previous).get(current);
         return distance;
     }
 
-    private double testRouteLength(List<Integer> route, int startDepotId, int customerSize, int newCustomer) {
+    private double testRouteLength(List<Integer> route, int startDepotId, int newCustomer) {
 
         double distance = 0;
         //This is the index of the starting depot in the neighbour matrix
-        int previous = customerSize-1 + startDepotId;
+        int previous = this.customers.size()-1 + startDepotId;
         //Calculates the distance from start depot to the new customer
         int current = newCustomer;
         distance += neightbourMatrix.get(previous).get(current);
@@ -135,10 +149,10 @@ public class DNA {
         return distance;
     }
 
-    private double testRouteWeight(Map<Integer, Customer> customers, List<Integer> route, Customer newCustomer){
+    private double testRouteWeight(List<Integer> route, Customer newCustomer){
         double totalWeight = 0;
         for(int i = 0; i < route.size(); i++){
-            Customer currentCustomer = customers.get(i);
+            Customer currentCustomer = this.customers.get(i);
             totalWeight += currentCustomer.getWeight();
         }
         totalWeight += newCustomer.getWeight();
@@ -146,14 +160,14 @@ public class DNA {
     }
 
     public List<List<Integer>> getDNAString() {
-        return DNAString;
+        return this.DNAString;
     }
 
     public double getTotalDistance() {
-        return totalDistance;
+        return this.totalDistance;
     }
 
     public double getFitness() {
-        return fitness;
+        return this.fitness;
     }
 }
--- Project1-GA/src/Run.java
+++ Project1-GA/src/Run.java
@@ -1,15 +1,22 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Run {
     private int initialPopulation = 1000;
     private double crossoverRate = 1;
     private double mutationRate = 1;
-    private int maxGenerationNumber = 1000;
+    private int maxGenerationNumber = 1;
     private int generationNumber = 0;
     private double targetFitness = 0;
     private double currentBestFitness = 0;
+    private List<Integer> individualIndexes;
+
+
 
     private List<DNA> population = new ArrayList<>();
 
@@ -19,13 +26,67 @@ public class Run {
         DataReader dr = new DataReader();
         dr.readFile("data/Data Files/" + fileName);
         this.targetFitness = dr.resultReader("data/Solution Files/" + fileName + ".res");
-        for (int i = 0; i < this.maxGenerationNumber; i++) {
+        for (int i = 0; i < this.initialPopulation; i++) {
             DNA dna = new DNA(dr.vehicle_dict, dr.depot_dict, dr.customer_dict);
             this.population.add(dna);
         }
 
-        while (this.generationNumber <= this.maxGenerationNumber || currentBestFitness*1.05 > targetFitness){
-            break;
+        while (this.generationNumber <= this.maxGenerationNumber && currentBestFitness < targetFitness*1.05){
+            this.individualIndexes = IntStream.rangeClosed(0, this.population.size()-1)
+                    .boxed().collect(Collectors.toList());
+            sortPopulationfFitness(this.population);
+            this.currentBestFitness = this.population.get(0).getFitness();
+
+            //Logic for playing tournaments
+            int tournamentRounds = this.population.size()/2;
+            int participantNr = 10;
+            List<DNA> newPopulation = new ArrayList<>();
+
+            for(int j = 0; j < tournamentRounds; j++){
+                Collections.shuffle(this.individualIndexes);
+                List<DNA> participants = new ArrayList<>();
+                for(int k = 0; k < participantNr; k++){
+                    participants.add(this.population.get(this.individualIndexes.get(k)));
+                }
+                //returns two fittest in participants
+                List<DNA> winners = this.playTurnament(participants);
+
+                //Crossover the two winners to create two parents and add them to the new population
+                List<DNA> children = this.crossover(winners);
+                newPopulation.addAll(children);
+            }
+            for(int l = 0; l < this.population.size()/2; l++){
+                newPopulation.add(population.get(l));
+            }
+            this.mutate(newPopulation);
+            this.population = newPopulation;
+
+            this.generationNumber += 1;
+        }
+        Visualizer vis = new Visualizer(dr.depot_dict, dr.customer_dict, dr.vehicle_dict,
+                this.population.get(0).getDNAString(), dr.maxCoordinate, dr.minCoordinate);
+    }
+
+    private void sortPopulationfFitness(List<DNA> population) {
+        //Sort list in ascending order
+        population.sort(Comparator.comparingDouble(DNA::getFitness));
+    }
+
+    private List<DNA> playTurnament(List<DNA> participants){
+        List<DNA> winners = new ArrayList<>();
+        sortPopulationfFitness(participants);
+        winners.add(participants.get(0));
+        winners.add(participants.get(1));
+        return winners;
+    }
+
+    private List<DNA> crossover(List<DNA> parents){
+        return parents;
+    }
+
+    private void mutate(List<DNA> population){
+        for(DNA individual: population){
+
         }
     }
 
