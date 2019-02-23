82f9ee29d9b108f38a15da121fe5dae6e7e774fe
unknown <alfredronning@gmail.com>
2019-02-23 17:22:30 +0100

    make edge class

--- Project2-MOEA/src/Edge.java
+++ Project2-MOEA/src/Edge.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.awt.*;
 
 public class Edge {
+    private Color from;
+    private Color to;
+    private double distance;
+
+    public Edge(Color from, Color to, float distance) {
+        this.from = from;
+        this.to = to;
+        this.distance = distance;
+    }
+
+    public Color getFrom() {
+        return from;
+    }
+
+    public Color getTo() {
+        return to;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
 }
--- Project2-MOEA/src/ImageMat.java
+++ Project2-MOEA/src/ImageMat.java
@@ -1,4 +1,4 @@
 package PACKAGE_NAME;
 
-public class image {
+public class ImageMat {
 }
