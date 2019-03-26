b27f5b62f9125dafc4698f5831b401b2131175b7
alfredronning <alfredronning@gmail.com>
2019-03-26 19:20:41 +0100

    add some parameters to settings

--- Project3-JSSP/src/Settings.java
+++ Project3-JSSP/src/Settings.java
@@ -1,2 +1,15 @@
 public class Settings {
+    // init
+    public static final double xmin = 0;
+    public static final double max = 2;
+    public static final double vmin = -2;
+    public static final double vmax = 2;
+
+
+    // exploration exploitation
+    public static final double c1 = 1;
+    public static final double c2 = 1;
+    public static final double inertiaWeight = 0.8;
+    public static final double getInertiaWeightDecrementFactor = 0.1;
+
 }
