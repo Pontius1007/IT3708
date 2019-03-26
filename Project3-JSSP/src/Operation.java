9fcbd5666fecadc4cf0abe16291a2bed937350b4
alfredronning <alfredronning@gmail.com>
2019-03-26 19:17:18 +0100

    change name of job class to operation

---
+++ Project3-JSSP/src/Operation.java
@@ -0,0 +1,26 @@
+public class Operation implements Comparable<Operation>{
+    public int jobId;
+    public int machineId;
+    public double position;
+    public double velocity;
+
+    public Operation(int jobId, int machineId, double position, double velocity) {
+        this.jobId = jobId;
+        this.machineId = machineId;
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    public Operation(int jobId, int machineId, double position) {
+        this.jobId = jobId;
+        this.machineId = machineId;
+        this.position = position;
+    }
+
+    @Override
+    public int compareTo(Operation o) {
+        if(this.position < o.position) return -1;
+        else if(this.position > o.position) return 1;
+        return 0;
+    }
+}
