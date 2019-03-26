e2b56a90e94e66049adfa9007f0dd86f1819917a
alfredronning <alfredronning@gmail.com>
2019-03-26 23:03:11 +0100

    some random settings

--- Project3-JSSP/src/Settings.java
+++ Project3-JSSP/src/Settings.java
@@ -12,4 +12,11 @@ public class Settings {
     public static final double inertiaWeight = 0.8;
     public static final double getInertiaWeightDecrementFactor = 0.1;
 
+    // PSO parameters
+    public static final int swarmSize = 40;
+    public static final int numberOfGenerations = 10000;
+    public static final String testData = "1";
+
+    public static final boolean verbose = true;
+
 }
