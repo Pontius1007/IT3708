3a1d91d5b4e278daeafc8dbf66202634e67614e2
alfredronning <alfredronning@gmail.com>
2019-03-27 01:11:54 +0100

    formatting

--- Project3-JSSP/src/PSO.java
+++ Project3-JSSP/src/PSO.java
@@ -17,47 +17,50 @@ public class PSO {
         }
     }
 
-    public void run(){
+    public void run() {
         // initiate swarm
         swarm = new Particle[Settings.swarmSize];
-        for(int i = 0; i < Settings.swarmSize; i++){
+        for (int i = 0; i < Settings.swarmSize; i++) {
             swarm[i] = new Particle();
         }
         globalBest = swarm[0];
 
-        for(int generation = 0; generation < Settings.numberOfGenerations; generation++){
+        for (int generation = 0; generation < Settings.numberOfGenerations; generation++) {
             // update global best particle
-            for(Particle p: swarm){
-                if(p.makespan < globalBest.makespan){
+            for (Particle p : swarm) {
+                if (p.makespan < globalBest.makespan) {
                     globalBest = new Particle(p);
                 }
             }
             // update particle position and velocities
-            for(Particle p: swarm){
+            for (Particle p : swarm) {
                 p.updateParticle(globalBest);
             }
-            if(Settings.verbose){
+            if (Settings.verbose) {
                 System.out.println(" ");
                 System.out.println(" ");
-                System.out.println("Generation nr. "+generation);
+                System.out.println("Generation nr. " + generation);
                 System.out.println("Best makespan: " + globalBest.makespan);
                 System.out.println(" ");
                 System.out.println(" ");
             }
 
-            if(Settings.inertiaWeight > Settings.inertiaWeightLowerBound)
-            Settings.inertiaWeight *= Settings.inertiaWeightDecrementFactor;
+            if (Settings.inertiaWeight > Settings.inertiaWeightLowerBound)
+                Settings.inertiaWeight *= Settings.inertiaWeightDecrementFactor;
         }
 
         Schedule bestSchedule = new Schedule(globalBest.particle);
-        for(List<Integer> machine: bestSchedule.schedule){
+        for (List<Integer> machine : bestSchedule.schedule) {
             System.out.println(machine);
         }
 
     }
 
     public static void main(String[] args) {
+        final long startTime = System.currentTimeMillis();
         PSO test = new PSO();
         test.run();
+        final long endTime = System.currentTimeMillis();
+        System.out.println("Execution time: " + (endTime - startTime));
     }
 }
