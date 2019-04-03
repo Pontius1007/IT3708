0811d20fb67394c754346350cd337335fb2786fe
unknown <alfredronning@gmail.com>
2019-04-03 02:06:37 +0200

    visualizer error

--- Project3-JSSP/src/Visualizer.java
+++ Project3-JSSP/src/Visualizer.java
@@ -1,22 +1,3 @@
-import java.time.LocalDate;
-import java.time.ZoneOffset;
-import java.util.Date;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-import javax.swing.WindowConstants;
-
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.data.category.IntervalCategoryDataset;
-import org.jfree.data.gantt.Task;
-import org.jfree.data.gantt.TaskSeries;
-import org.jfree.data.gantt.TaskSeriesCollection;
-
-/**
- * @author imssbora
- *
- */
-public class Visualizer extends JFrame {
+public class Visualizer {
 }
