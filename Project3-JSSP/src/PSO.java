9f5bc8f2ac681bc7a85aed17c8077a0cd9f758ea
Pontius1007 <embrikeinang@msn.com>
2019-04-11 13:36:55 +0200

    Passes numberOfJobs

--- Project3-JSSP/src/PSO.java
+++ Project3-JSSP/src/PSO.java
@@ -77,7 +77,7 @@ public class PSO {
     }
 
     private void plottGantt(String title, Schedule ganttSchedule, Double makespan) {
-        final Visualizer gantt = new Visualizer(title, ganttSchedule, makespan);
+        final Visualizer gantt = new Visualizer(title, ganttSchedule, makespan, LookupTable.numberOfJobs);
         gantt.pack();
         RefineryUtilities.centerFrameOnScreen(gantt);
         gantt.setVisible(true);
